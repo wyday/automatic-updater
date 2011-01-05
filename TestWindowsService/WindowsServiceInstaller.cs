4288b3af30377fefcad3e33b527702a9a9358724
wyattoday <wyatt@wyday.com>
2011-01-05 21:31:10 +0000

    Add a Windows Service example

---
+++ TestWindowsService/WindowsServiceInstaller.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace WindowsService
+{
+    [RunInstaller(true)]
+    public class WindowsServiceInstaller : Installer
+    {
+        /// <summary>
+        /// Public Constructor for WindowsServiceInstaller.
+        /// - Put all of your Initialization code here.
+        /// </summary>
+        public WindowsServiceInstaller()
+        {
+            ServiceProcessInstaller serviceProcessInstaller = new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //# Service Account Information
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //# Service Information
+            serviceInstaller.DisplayName = "Test AutoUpdate Service in C#";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            // This must be identical to the WindowsService.ServiceBase name
+            // set in the constructor of WindowsService.cs
+            serviceInstaller.ServiceName = "Test AutoUpdate Service";
+
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
