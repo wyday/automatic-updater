da6caf27d503c2647fb7bba44a7e25a7d7e1b788
wyattoday <wyatt@wyday.com>
2012-02-04 03:55:11 +0000

    Let users specify their own way to exit their application using the new CloseAppNow event. This fixes problems with certain VSTO apps. (See: http://wyday.com/forum/viewtopic.php?f=1&p=8383#p8383 )

--- Control/AutomaticUpdater.cs
+++ Control/AutomaticUpdater.cs
@@ -23,7 +23,7 @@ namespace wyDay.Controls
     [ToolboxBitmapAttribute(typeof(AutomaticUpdater), "update-notify.png")]
     public class AutomaticUpdater : ContainerControl, ISupportInitialize
     {
-        readonly AutomaticUpdaterBackend auBackend = new AutomaticUpdaterBackend(true) { UseCloseAppNow = true };
+        readonly AutomaticUpdaterBackend auBackend = new AutomaticUpdaterBackend(true);
 
         Form ownerForm;
 
@@ -92,6 +92,13 @@ namespace wyDay.Controls
         [Description("Event is raised when the checking for updates fails."),
         Category("Updater")]
         public event FailHandler CheckingFailed;
+
+        /// <summary>
+        /// Event is raised after you or your user invoked InstallNow(). You should close your app as quickly as possible (because wyUpdate will be waiting).
+        /// </summary>
+        [Description("Event is raised after you or your user invoked InstallNow(). You should close your app as quickly as possible (because wyUpdate will be waiting)."),
+        Category("Updater")]
+        public event EventHandler CloseAppNow;
 
         /// <summary>
         /// Event is raised when an update can't be installed and the closing is aborted.
@@ -498,7 +505,10 @@ namespace wyDay.Controls
             }
 
             // close this application so it can be updated
-            Application.Exit();
+            if (CloseAppNow != null)
+                CloseAppNow(this, EventArgs.Empty);
+            else
+                Application.Exit();
         }
 
         void auBackend_UpToDate(object sender, SuccessArgs e)
